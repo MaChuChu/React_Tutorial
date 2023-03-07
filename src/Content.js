4e24380e7ce0aa338b79fd1fe521d2697ad5b3e2
Ranjeth Ravichandran <ranjethravichandran@hotmail.com>
2023-03-07 16:13:05 +0000

    Chapter 8 & 9 (Complete)

--- src/Content.js
+++ src/Content.js
@@ -1,64 +1,15 @@
+import ItemList from './ItemList';
 
-import { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
-
-const Content = () => {
-    
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: false,
-            item: "One hald pound bag of Cocoa Covered Alomonds Unsalted"
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "Item 2"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Item 3"
-        }
-    ]);
-
-    const handleCheck = (id) => {
-        // Create an array of items, and flips items checked.
-        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
-
-    const handleDelete = (id) => {
-        // Creates an array without the passed item.
-        const listItems = items.filter((item) => item.id !== id);
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
+const Content = ({ items, handleCheck, handleDelete }) => {
 
     return (
         <main>
             {items.length ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className="item" key={item.id}>
-                            <input 
-                                type="checkbox" 
-                                onChange={() => handleCheck(item.id)} 
-                                checked={item.checked}
-                            />
-                            <label
-                                style={(item.checked) ? { textDecoration: 'line-through'} : null} 
-                                onDoubleClick={() => handleCheck(item.id)}
-                            >{item.item}</label>
-                            <FaTrashAlt 
-                                onClick={() => handleDelete(item.id)}
-                                role={"button"} 
-                                tabIndex="0" 
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ) : (
                 <p>List is empty...</p>
             )}
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 
-const Footer = () => {
-    const today = new Date();
+const Footer = ({ length }) => {
+    // const today = new Date();
     return (
         <footer>
-            <p>Copyright &copy; {today.getFullYear()}</p>
+            {/* <p>Copyright &copy; {today.getFullYear()}</p> */}
+            <p>{length} : List {length === 1 ? "Item" : "Items"}</p>
         </footer>
     )
 }
